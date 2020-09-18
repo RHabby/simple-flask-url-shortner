fa9faf349f52ece8e4517bcf462eb7997c356942
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2020-09-18 14:12:24 +0300

    csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 from flask_migrate import Migrate
 
+csrf = CSRFProtect()
 db = SQLAlchemy()
 migrate = Migrate()
-
-from app.url_shortener.models import Link
 
 
 def create_app(config="config.py"):
     flask_app = Flask(__name__)
     flask_app.config.from_pyfile(config)
 
+    csrf.init_app(flask_app)
     db.init_app(flask_app)
     migrate.init_app(app=flask_app, db=db)
 
-    from app.views import blueprint as test
-    flask_app.register_blueprint(blueprint=test)
-
     from app.url_shortener.views import blueprint as url_shortener
     flask_app.register_blueprint(blueprint=url_shortener)
+    # register_bps(flask_app)
 
     return flask_app
+
+
+def register_bps(flask_app):
+    from app.url_shortener.views import blueprint as url_shortener
+    flask_app.register_blueprint(blueprint=url_shortener)
