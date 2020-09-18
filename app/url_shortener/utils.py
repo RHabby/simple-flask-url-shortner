336dab009c7c17d88c109f0665de59ece7b69275
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2020-09-18 14:09:31 +0300

    func was redefined in forms.py

--- app/url_shortener/utils.py
+++ app/url_shortener/utils.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 import requests
 
 
+# moved to forms.py with wtforms
 def validate_url(url: str) -> bool:
     url = url.strip()
 
@@ -14,10 +15,18 @@ def validate_url(url: str) -> bool:
     domain = parsed_url.netloc
 
     if not all([scheme, domain]):
+        if scheme == "":
+            url = f"https://{url}"
+            return validate_url(url=url)
         return False
 
-    r = requests.head(url)
-    if r.status_code != 200:
-        return False
+    r = requests.get(url)
+    status = r.status_code
+    print(scheme, domain, url, status)
 
-    return True
+    if str(status).startswith("3"):
+        return validate_url(url=r.headers["Location"])
+    if status != 200:
+        return url, False
+
+    return url, True
